daeed76c35b43daef06fb8870bf0894c4f6c65df
charles <1140601003@qq.com>
2023-06-02 13:41:07 +0800

    vault backup: 2023-06-02 13:41:07

--- source/python/getinput.py
+++ source/python/getinput.py
@@ -1,15 +1,29 @@
 import os
+from mdbanner import MarkdownManipulator
+from config import current_directory
+_posts_path = current_directory +"\\_posts"
+_photo_path = current_directory+'\\images' 
+
 
 filename = '未命名.md'
-content_to_add = '1111'
+markdown_manipulator = MarkdownManipulator(filename)
+content_to_add = markdown_manipulator.add_title(_photo_path)
 
-# 获取当前文件夹路径
-current_directory = os.getcwd()+"\source\python"
 
-# 遍历当前文件夹中的文件
-for file in os.listdir(current_directory):
-    if file == filename:
-        # 找到目标文件
-        file_path = os.path.join(current_directory, file)
-        with open(file_path, 'r+', encoding='utf-8') as f:
-            f.write(content_to_add)
+
+def traverse_files(directory):
+    # 遍历当前文件夹中的文件
+    for file in os.listdir(directory):
+        file_path = os.path.join(directory, file)
+        if os.path.isfile(file_path) and file == filename:
+            # 找到目标文件
+            with open(file_path, 'r+', encoding='utf-8') as f:
+                f.seek(0, 0)  # 将文件指针移到文件开头
+                f.write(content_to_add + '\n')
+        elif os.path.isdir(file_path):
+            # 递归遍历子文件夹
+            traverse_files(file_path)
+
+
+traverse_files(current_directory)
+
---
+++ source/python/mdbanner.py
@@ -0,0 +1,29 @@
+import os
+import random
+from datetime import datetime
+
+formatted_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+class MarkdownManipulator:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def add_title(self, image_folder):
+        # 获取指定文件夹中的所有图片文件
+        image_files = [f for f in os.listdir(image_folder) if os.path.isfile(os.path.join(image_folder, f)) and 'banne_photo' in f]
+
+        # 随机选择一张图片作为new_index_img
+        if image_files:
+            new_index_img = random.choice(image_files)
+        else:
+            # 如果没有符合条件的图片，则使用默认值
+            new_index_img = 'default.png'
+            
+        title = f'---\n \
+title: \n \
+date: {formatted_datetime}\n \
+tags: []\n \
+index_img: ../images/{new_index_img}\n \
+---\n'
+        return title
+        
