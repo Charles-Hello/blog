78306d75305804cc6359e69d64fb7effab949c12
charles <1140601003@qq.com>
2023-06-02 17:01:59 +0800

    vault backup: 2023-06-02 17:01:59

--- source/python/getinput.py
+++ source/python/getinput.py
@@ -3,7 +3,9 @@ from mdbanner import MarkdownManipulator
 from config import posts_path, photo_path
 
 
+
 filename = '未命名.md'
+new_filename = f'{1}.md'  # 新的文件名
 markdown_manipulator = MarkdownManipulator(filename)
 content_to_add = markdown_manipulator.add_title(photo_path)
 
@@ -15,7 +17,9 @@ def traverse_files(directory):
         file_path = os.path.join(directory, file)
         if os.path.isfile(file_path) and file == filename:
             # 找到目标文件
-            with open(file_path, 'r+', encoding='utf-8') as f:
+            new_file_path = os.path.join(directory, new_filename)
+            os.rename(file_path, new_file_path)  # 重命名文件
+            with open(new_file_path, 'r+', encoding='utf-8') as f:
                 f.seek(0, 0)  # 将文件指针移到文件开头
                 f.write(content_to_add + '\n')
         elif os.path.isdir(file_path):
@@ -24,4 +28,5 @@ def traverse_files(directory):
 
 
 traverse_files(posts_path)
+
 
